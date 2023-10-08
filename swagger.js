da08ead7fac2626446047e983ad92c11a3440329
Ednilson <ednilsoncastanheira53@gmail.com>
2023-10-08 05:02:55 +0100

    changed to https

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: "API that handles a database of contacts",
   },
   host: "https://cse341-assignment1-q09s.onrender.com",
-  schemes: ["http"],
+  schemes: ["https"],
 };
 
 const outputFile = "./swagger-output.json";
